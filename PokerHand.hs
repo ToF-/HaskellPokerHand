56b03cabe4d383101d5c17d4c6ef27f9f19a19a0
ToF- <cthibaut@octo.com>
2014-07-20 18:56:14 +0200

    added Card and Suit values and functions

--- PokerHand.hs
+++ PokerHand.hs
@@ -1,8 +1,37 @@
 module PokerHand
 where
+import Data.Char
 
-type Card = String
-data Rank = HighCard
+data Suit = Spade | Heart | Clover | Diamond
+    deriving (Show, Eq)
+data Card = Card Int Suit
+    deriving (Show, Eq)
+
+card :: String -> Card
+card [r,s] = Card (toValue r) (toSuit s)
+
+toValue :: Char -> Int
+toValue 'A' = 14
+toValue 'K' = 13
+toValue 'Q' = 12
+toValue 'J' = 11
+toValue 'T' = 10
+toValue c = (ord c) - (ord '0')
+
+toSuit :: Char -> Suit
+toSuit 'h' = Heart
+toSuit 's' = Spade
+toSuit 'c' = Clover
+toSuit 'd' = Diamond
+
+value :: Card -> Int
+value (Card v s) = v
+
+suit :: Card -> Suit
+suit (Card v s) = s
+
+type Rank = Integer
+data Hand = HighCard Rank
 
 scores :: [String] -> [String]
 scores = map score 
@@ -10,19 +39,18 @@ scores = map score
 score :: String -> String        
 score s = 
     let
-        cs = cards s
-        sc = case bestRank cs of 
-                Just r -> " " ++ show r ++ " (winner)"
-                Nothing -> ""
-    in s ++ sc
+        cs = map card (words s)
+        sc = showHandScore (bestHand cs)
+    in s ++ if (not (null sc)) then " "++ sc ++ " (winner)" else ""
         
-cards :: String -> [Card]
-cards = words
+instance Show Hand
+    where show (HighCard _) = "High Card"
 
-instance Show Rank
-    where show HighCard = "High Card"
-
-bestRank :: [Card] -> Maybe Rank
-bestRank cs = case length cs of
-                7 -> Just HighCard
+bestHand :: [Card] -> Maybe Hand
+bestHand cs = case length cs of
+                7 -> Just (HighCard 14)
                 _ -> Nothing
+
+showHandScore :: Maybe Hand -> String
+showHandScore (Just hand) = show hand
+showHandScore Nothing     = ""
--- Specs.hs
+++ Specs.hs
@@ -12,3 +12,23 @@ main = hspec $ do
 
         it "should rank the only High Card in a game as the winner" $ do
             scores ["Kc Qd Jc 8h 7h 5h 2s","Kc 2s"] `shouldBe`  ["Kc Qd Jc 8h 7h 5h 2s High Card (winner)","Kc 2s"]
+
+    describe "Card values" $ do
+        it "should be created from a string in the form [A|K|Q|J|T|9..2][h|s|c|d]" $ do
+            card "9h" `shouldBe` Card 9 Heart
+            card "5s" `shouldBe` Card 5 Spade 
+            card "3c" `shouldBe` Card 3 Clover
+            card "2d" `shouldBe` Card 2 Diamond
+
+        it "should be created with higher values for A,K,Q,J and T" $ do
+            card "Ah" `shouldBe` Card 14 Heart
+            card "Ks" `shouldBe` Card 13 Spade
+            card "Qc" `shouldBe` Card 12 Clover
+            card "Jd" `shouldBe` Card 11 Diamond
+            card "Ts" `shouldBe` Card 10 Spade
+
+        it "should extract its value" $ do
+            value (card "Qc") `shouldBe` 12
+
+        it "should extract its suit" $ do
+            suit (card "Qc") `shouldBe` Clover
