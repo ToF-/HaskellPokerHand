6f09591764ad250f00809fb83437214b90fb7b2d
ToF- <cthibaut@octo.com>
2014-07-23 23:58:51 +0200

    second test with different value and suit

--- PokerHand.hs
+++ PokerHand.hs
@@ -1,10 +1,15 @@
 module PokerHand
 where
 
-data Value = King   deriving (Show,Eq)
-data Suit  = Spade  deriving (Show,Eq)
+data Value = King | Ace
+    deriving (Show, Eq)
+
+data Suit  = Heart | Spade
+    deriving (Show, Eq)
+
 data Card  = Card Value Suit
     deriving (Show, Eq)
 
 card :: String -> Card
 card "Ks" = Card King Spade
+card "Ah" = Card Ace Heart
--- Specs.hs
+++ Specs.hs
@@ -6,3 +6,4 @@ main = hspec $ do
     describe "Cards" $ do
         it "should be extracted from Strings" $ do
             card "Ks" `shouldBe` Card King Spade
+            card "Ah" `shouldBe` Card Ace Heart
