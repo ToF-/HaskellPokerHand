4cff953991b50b1d6b4c2c7cb6d96bca7e29c860
ToF- <cthibaut@octo.com>
2014-07-26 09:13:30 +0200

    refactoring

--- PokerHand.hs
+++ PokerHand.hs
@@ -15,30 +15,31 @@ data Card  = Card { rank::Rank, suit::Suit }
 
 card :: String -> Card
 card [v,s] = Card (rankFromChar v) (suitFromChar s)
+    where 
+        rankFromChar :: Char -> Rank
+        rankFromChar '2' = Two
+        rankFromChar '3' = Three
+        rankFromChar '4' = Four
+        rankFromChar '5' = Five
+        rankFromChar '6' = Six
+        rankFromChar '7' = Seven
+        rankFromChar '8' = Eight
+        rankFromChar '9' = Nine
+        rankFromChar 'T' = Ten
+        rankFromChar 'J' = Jack
+        rankFromChar 'Q' = Queen
+        rankFromChar 'K' = King
+        rankFromChar 'A' = Ace
 
-rankFromChar :: Char -> Rank
-rankFromChar '2' = Two
-rankFromChar '3' = Three
-rankFromChar '4' = Four
-rankFromChar '5' = Five
-rankFromChar '6' = Six
-rankFromChar '7' = Seven
-rankFromChar '8' = Eight
-rankFromChar '9' = Nine
-rankFromChar 'T' = Ten
-rankFromChar 'J' = Jack
-rankFromChar 'Q' = Queen
-rankFromChar 'K' = King
-rankFromChar 'A' = Ace
+        suitFromChar :: Char -> Suit
+        suitFromChar 'h' = Hearts
+        suitFromChar 's' = Spades
+        suitFromChar 'd' = Diamonds
+        suitFromChar 'c' = Clubs
 
 cards :: String -> [Card]
 cards = (map card) . words
     
-suitFromChar :: Char -> Suit
-suitFromChar 'h' = Hearts
-suitFromChar 's' = Spades
-suitFromChar 'd' = Diamonds
-suitFromChar 'c' = Clubs
 
 data Hand = Fold
           | HighCard [Rank]
